31952b33e53e0758f5e522a759430abe62f1ad84
Kevin Lau <thekevlau@gmail.com>
2015-04-05 17:00:32 -0700

    handling stdin, adding basic unit test

--- spoj_practice/spoj2/spoj2.py
+++ spoj_practice/spoj2/spoj2.py
@@ -8,11 +8,9 @@ from sys import stdin
 # Find all primes between a given n, m
 
 
-n = 100
-m = 5
-
-# handle 1,2 case
 # handle stdin/stdout and unit testing
+
+
 
 # returns a list of all the primes in between
 def find_primes_between (n, m):
@@ -25,11 +23,15 @@ def find_primes_between (n, m):
     # 1 is not a prime number, the loop excludes even numbers thus 2 does not need to be added
     primes = [3]
     primes_between = []
+    
+    # unique cases
     if m < 2:
         primes_between.append(2)
-    if m < 3:
+    if m <= 3:
         primes_between.append(3)
-    for i in xrange(3, n, 2):
+    
+    # check all primes
+    for i in xrange(3, n+1, 2):
         # check if number is prime
         for prime in primes:
             # if is not prime, move onto next number i
@@ -37,20 +39,24 @@ def find_primes_between (n, m):
                 break
         # if the number is in fact prime,
         else:
-            print ("prime number identified: %s" % (i))
             primes.append(i)
             if i >= m:
-                print ("prime between n, m identified: %s" % (i))
                 primes_between.append(i)
+    
+    return primes_between 
 
-    print ("DONE!")
-    print ("All primes are as follows:")
+num_of_lines = int(stdin.readline())
+for i in xrange(num_of_lines):
+    # numbers = stdin.readline().split(" ")
+    # m = numbers[0]
+    # n = numbers[1]
+    # instead, do:
+    m, n = stdin.readline().split(' ')
+    m = int(m)
+    n = int(n)
+    prime = []
+    primes = find_primes_between(n, m)
     for prime in primes:
-        print prime
-    print ("DONE!")
-    print ("All primes in between are as follows:")
-    for prime in primes_between:
-        print prime
-
-find_primes_between(n, m)
-
+        print (prime)
+    # takes out extra space between each iteration
+    print ('\n'.strip())
