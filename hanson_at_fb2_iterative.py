9a8e8188481df82c31840d8e76b5e577b5c6a660
Kevin Lau <thekevlau@gmail.com>
2015-04-11 21:17:56 -0700

    fixed some variable names, cleaned up logic

--- hanson_at_fb2_iterative.py
+++ hanson_at_fb2_iterative.py
@@ -7,24 +7,24 @@
 # input [2], output [], [2], [1], [1, 2]
 # every time you add the new number to previously existing elements
 
-def subset_list (input_list):
-	output_list = [[]]
+def subset_list (int_list):
+	all_subsets = [[]]
 
-	for in_element in sorted(input_list):
-		total_list = []
-		for list_within in output_list:
-			# put the new element into a list of itself
-			storage_list = [in_element]
-			# make temp version which we can manipulate without affecting the original
-			temp_list_within = list(list_within)
-			# join the two to form the next combination of list items
-			temp_list_within += storage_list
-			# add it to a temp storage list of all combinations for this iteration
-			total_list.append(temp_list_within)
+	for input_integer in int_list:
+		subset = []
+		for known_subsets in all_subsets:
+			# route 1, make a temp version we can manipulate without affecting the original, append to it to make the new combination, then add it to the subset list for this iteration.
+			temp_known_subsets = list(known_subsets)
+			temp_known_subsets.append(input_integer)
+
+			# route 2, make the input_integer into a list so we can then add it to known_subsets, which is defined as a new list. this again allows us to concatenate both lists without modifying the original known_subsets in all_subsets
+			#list_of_input_int = [input_integer]
+			#temp_known_subsets = known_subsets + list_of_input_int 
+			subset.append(temp_known_subsets)
 		# add that temp storage list to the output list
-		output_list += total_list
+		all_subsets += subset
 
-	return output_list
+	return all_subsets
 
-input_list = [1, 2, 3, 4]
-print subset_list(input_list)
+int_list = [1, 2, 3, 4]
+print subset_list(int_list)
