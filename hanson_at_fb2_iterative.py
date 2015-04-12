30578a1de4d04db60bf36c6aa70dec5c4972ebcc
Kevin Lau <thekevlau@gmail.com>
2015-04-11 21:52:33 -0700

    it only takes one line, not two

--- hanson_at_fb2_iterative.py
+++ hanson_at_fb2_iterative.py
@@ -14,12 +14,12 @@ def subset_list (int_list):
 		subset = []
 		for known_subsets in all_subsets:
 			# route 1, make a temp version we can manipulate without affecting the original, append to it to make the new combination, then add it to the subset list for this iteration.
-			temp_known_subsets = list(known_subsets)
-			temp_known_subsets.append(input_integer)
+			#temp_known_subsets = list(known_subsets)
+			#temp_known_subsets.append(input_integer)
 
 			# route 2, make the input_integer into a list so we can then add it to known_subsets, which is defined as a new list. this again allows us to concatenate both lists without modifying the original known_subsets in all_subsets
 			#list_of_input_int = [input_integer]
-			#temp_known_subsets = known_subsets + list_of_input_int 
+			temp_known_subsets = known_subsets + [input_integer] 
 			subset.append(temp_known_subsets)
 		# add that temp storage list to the output list
 		all_subsets += subset
