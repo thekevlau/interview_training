26e9552ba326d6d3a48574618708771f37e3f82a
Kevin Lau <thekevlau@gmail.com>
2015-04-23 14:23:56 -0700

    replaced second loop with map function

--- hanson_at_fb2_iterative.py
+++ hanson_at_fb2_iterative.py
@@ -10,21 +10,28 @@
 def subset_list (int_list):
 	all_subsets = [[]]
 
-	for input_integer in int_list:
-		subset = []
-		for known_subsets in all_subsets:
+	for integer in int_list:
+		# subsets = []
+		#for known_subsets in all_subsets:
 			# route 1, make a temp version we can manipulate without affecting the original, append to it to make the new combination, then add it to the subset list for this iteration.
 			#temp_known_subsets = list(known_subsets)
-			#temp_known_subsets.append(input_integer)
+			#temp_known_subsets.append(integer)
 
-			# route 2, make the input_integer into a list so we can then add it to known_subsets, which is defined as a new list. this again allows us to concatenate both lists without modifying the original known_subsets in all_subsets
-			#list_of_input_int = [input_integer]
-			temp_known_subsets = known_subsets + [input_integer] 
-			subset.append(temp_known_subsets)
+			# route 2, make the integer into a list so we can then add it to known_subsets, which is defined as a new list. this again allows us to concatenate both lists without modifying the original known_subsets in all_subsets
+			#list_of_input_int = [integer]
+ 
+			#subsets.append(temp_known_subsets)
 		# add that temp storage list to the output list
-		all_subsets += subset
+		#all_subsets += subsets
+
+		# map returns a list
+		# map maps a function to an iterable
+		# the varaibles passed into lambda come from ONLY the iterable that is passed into the map function.
+		# anything else you can just access directly without passing it into lambda, in fact you cannot pass them into lambda. 
+		subsets = map(lambda known_subsets: known_subsets + [integer], all_subsets)
+		all_subsets += subsets
 
 	return all_subsets
 
-int_list = [1, 2, 3, 4]
+int_list = [1, 2, 3]
 print subset_list(int_list)
